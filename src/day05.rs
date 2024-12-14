2a1b23f3313e6c03fd9bf7f4d2958cb958f74ac8
Agustinus Kristiadi <agustinus@kristia.de>
2024-12-14 17:09:34 -0500

    Add solution 2 of day 5

--- src/day05.rs
+++ src/day05.rs
@@ -3,10 +3,9 @@ use std::fs;
 
 pub fn solve() -> Result<()> {
     let input: String = fs::read_to_string("data/day05_input.txt")?;
-    // let input: String = fs::read_to_string("data/day05_input_toy.txt")?;
 
     println!("Day 5 Part 1: {}", part1(input.clone()));
-    // println!("Day 5 Part 2: {}", part12(input.clone(), 6));
+    println!("Day 5 Part 2: {}", part2(input.clone()));
 
     Ok(())
 }
@@ -37,7 +36,42 @@ fn part1(input: String) -> usize {
                 .sum();
 
             let ok = (vowel_count >= 3) && (repeat_count > 0) && (not_containing_count == 0);
+            ok as usize
+        })
+        .sum()
+}
 
+fn part2(input: String) -> usize {
+    input
+        .trim()
+        .split('\n')
+        .map(|line| {
+            // Requirement 1
+            let mut repeat_ok = false;
+
+            for i in 1..line.len() {
+                let c1 = line.chars().nth(i - 1).unwrap();
+                let c2 = line.chars().nth(i).unwrap();
+
+                if let Some(_) = line.split_at(i + 1).1.find(&String::from_iter([c1, c2])) {
+                    repeat_ok = true;
+                    break;
+                }
+            }
+
+            // Requirement 2
+            let mut between_ok = false;
+
+            for chars in line.chars().collect::<Vec<char>>().windows(3) {
+                if let [a, _, c] = chars {
+                    if a == c {
+                        between_ok = true;
+                        break;
+                    }
+                }
+            }
+
+            let ok = repeat_ok && between_ok;
             ok as usize
         })
         .sum()
